2d2bf3a8e0cdd561f3ae57cb16ea5bd88308d4bf
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2023-05-01 19:30:36 -0300

    Create Styles

--- Rico_Quimico/App.js
+++ Rico_Quimico/App.js
@@ -1,7 +1,26 @@
 import React from "react";
 import {SplashScreen} from './src/screens/SplashScreen'
+import AppLoading from 'expo-app-loading'
+import {
+  useFonts,
+  SourceSansPro_400Regular,
+  SourceSansPro_700Bold,
+  SourceSansPro_600SemiBold,
+  SourceSansPro_900Black,
+} from '@expo-google-fonts/source-sans-pro'
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    SourceSansPro_400Regular,
+    SourceSansPro_700Bold,
+    SourceSansPro_600SemiBold,
+    SourceSansPro_900Black,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <SplashScreen/>
   );
